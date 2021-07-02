5f1e28ef8ebd7a72c7b472fff7d4628533aefda8
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2021-07-02 12:41:34 +0600

    Sanctum auth logic updated

--- Laravel--SanctumFortify--Auth~(Done)/nuxt-client-sanctum/pages/people.vue
+++ Laravel--SanctumFortify--Auth~(Done)/nuxt-client-sanctum/pages/people.vue
@@ -1,35 +1,42 @@
 <template>
     <div class="m-10">
-        <h1 class="text-2xl font-bold">
-            User List:
-        </h1>
-        <ul v-if="usersList && Object.keys(usersList).length">
-            <li v-for="user in usersList" :key="user.id">
-                <span class="font-bold">Name: </span>{{ user.name }},
-                <span class="font-bold">Username: </span>{{ user.username }},
-                <span class="font-bold">Email: </span> {{ user.email }}
-            </li>
-        </ul>
+        <div v-if="usersList && Object.keys(usersList).length">
+            <h1 class="text-2xl font-bold">
+                User List:
+            </h1>
+            <ul>
+                <li v-for="user in usersList" :key="user.id" class="border list-none rounded-sm px-3 py-3" style='border-bottom-width:0'>
+                    <span class="font-bold">Name: </span>{{ user.name }},
+                    <span class="font-bold">Username: </span>{{ user.username }},
+                    <span class="font-bold">Email: </span> {{ user.email }}
+                </li>
+            </ul>
+        </div>
+        <div v-else>
+            <div style="text-align: center; padding-top: 20px; color: blueviolet; font-weight: bolder;">
+                No Users found ...
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     middleware: ['auth'],
-    data(){
-        return{
-            usersList:{}
+    data() {
+        return {
+            usersList: {}
         }
     },
     mounted() {
-      this.getUsers();
+        this.getUsers();
     },
-    methods:{
-        async getUsers(){
-            this.$axios.$get('/api/users').then((res)=>{
+    methods: {
+        async getUsers() {
+            this.$axios.$get('/api/users').then((res) => {
                 this.usersList = res;
-            }).catch((err)=>{
-                if(err){
+            }).catch((err) => {
+                if (err) {
                     alert('Error');
                     console.log(err);
                 }
--- Laravel--SanctumFortify--Auth~(Done)/vue2-client-sanctum/src/views/people.vue
+++ Laravel--SanctumFortify--Auth~(Done)/vue2-client-sanctum/src/views/people.vue
@@ -1,24 +1,31 @@
 <template>
-    <v-card
-        class="mx-auto"
-        max-width="400"
-        tile
-        v-if="users && users.length"
-    >
-        <div style="padding: 10px 0; text-align: center;background-color: bisque">User List</div>
-        <hr>
-        <v-list-item v-for="user in users" :key="user.id">
-            <v-list-item-content>
-                <v-list-item-title>{{ user.name }}</v-list-item-title>
-                <v-list-item-subtitle>
-                    {{ user.username }}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                    {{ user.email }}, <span v-if="user.email_verified_at" style="color: green; margin-left: 20px">Verified</span>
-                </v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-    </v-card>
+    <div>
+        <v-card
+            class="mx-auto"
+            max-width="400"
+            tile
+            v-if="users && users.length"
+        >
+            <div style="padding: 10px 0; text-align: center;background-color: bisque">User List</div>
+            <hr>
+            <v-list-item v-for="user in users" :key="user.id">
+                <v-list-item-content>
+                    <v-list-item-title>{{ user.name }}</v-list-item-title>
+                    <v-list-item-subtitle>
+                        {{ user.username }}
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle>
+                        {{ user.email }}, <span v-if="user.email_verified_at" style="color: green; margin-left: 20px">Verified</span>
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-card>
+        <div v-else>
+            <div style="text-align: center; padding-top: 20px; color: blueviolet; font-weight: bolder;">
+                No Users found ...
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -39,7 +46,6 @@ export default {
             await HTTP.get('/api/users')
                 .then((response) => {
                     this.users = response.data;
-                    console.log(this.users)
                 })
                 .catch(error => {
                     //generateErrors({commit, dispatch}, error, err, "Error Logging out!", false);
